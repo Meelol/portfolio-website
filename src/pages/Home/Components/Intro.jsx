1ffef36e5de7dfe15710882ace1ae9de37721b2c
Omar Rodriguez <eomarrodriguez@outlook.com>
2023-07-13 12:36:25 -0700

    Change description font size

--- src/pages/Home/Components/Intro.jsx
+++ src/pages/Home/Components/Intro.jsx
@@ -8,10 +8,10 @@ function Intro() {
             <h1 className='animate-fade-down animate-once animate-delay-[500ms] animate-ease-linear animate-normal animate-fill-backwards text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold'>
                 Edgar Rodriguez
             </h1>
-            <p className="animate-fade-right animate-once animate-delay-[1000ms] animate-ease-linear animate-normal animate-fill-backwards text-base md:text-xl mb-3 font-medium">
+            <p className="animate-fade-right animate-once animate-delay-[1000ms] animate-ease-linear animate-normal animate-fill-backwards text-xl md:text-xl mb-3 font-medium">
                     Software Engineer & Web Developer
             </p>
-            <p className='animate-fade-left animate-once animate-delay-[1500ms] animate-ease-linear animate-normal animate-fill-backwards text-sm max-w-xl mb-6 font-bold'>
+            <p className='animate-fade-left animate-once animate-delay-[1500ms] animate-ease-linear animate-normal animate-fill-backwards text-base max-w-xl mb-6 font-bold'>
                     Welcome to my portfolio website! 
                     I am Edgar Rodriguez, a recent computer science graduate
                     from the California State Polytechnic University of Pomona, 
